726257e9a13f33461f9e09f1c881666f0e1d2d75
artasvit <artem5646737@gmail.com>
2025-03-30 19:36:11 -0300

    Update bio.js

--- bio.js
+++ bio.js
@@ -7,12 +7,12 @@ const members = {
     2: {
         name: "Tu nombre",
         bio: "Tu biografia...",
-        img: "avatar2.jpg"
+        //img: "avatar2.jpg"
     },
     3: {
         name: "Tu nombre",
         bio: "Tu biografia...",
-        img: "avatar3.jpg"
+        //img: "avatar3.jpg"
     }
 };
 
