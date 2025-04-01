951e37b34992654c0ea8828ddc2041b5593015b6
artasvit <artem5646737@gmail.com>
2025-04-01 15:23:53 -0300

    Update bio.js

--- bio.js
+++ bio.js
@@ -6,7 +6,7 @@ const members = {
     },
     2: {
         name: "Mateo",
-        bio: "Hola, mi nombre es Mateo, me gusta comer empanadas, jugar Brawl Stars y competir en Jerkmate. También me encanta rapear y ya he grabado varios discos.",
+        bio: "rs y competir en Jerkmate. También me encanta rapear y ya he grabado varios discos.",
         img: "avatar2.jpg"
     },
     3: {
